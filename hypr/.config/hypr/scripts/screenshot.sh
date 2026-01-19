f2d97db0b394d8d64d2d2e18b1bfbdaa9b2cd880
Antonin Brauer <antonin.brauer@yahoo.fr>
2026-01-19 21:36:09 +0100

    update screenshot so that it freezes

--- hypr/.config/hypr/scripts/screenshot.sh
+++ hypr/.config/hypr/scripts/screenshot.sh
@@ -8,15 +8,11 @@ OUTPUT="~/Pictures/Screenshot/$(date '+%d-%m-%y_%H:%M:%S').png"
 
 mkdir -p ~/Pictures/Screenshot/
 
-if [ -z $1 ]; then
-    REGION="$(slurp || true)"
-    if [ -z $REGION ]; then
-	sleep 0.1 && grim -c $TEMP_PATH
-    else
-	grim -c -g "$REGION" $TEMP_PATH 
-    fi
-else # Assume `instant`
-    grim -c $TEMP_PATH
-fi
+REGION="$(still -c "slurp || true")"
+if [ -z $REGION ]; then
+    sleep 0.1 && grim -c $TEMP_PATH
+else
+    grim -c -g "$REGION" $TEMP_PATH 
+fi	
 
 satty --filename $TEMP_PATH --output-filename $OUTPUT
