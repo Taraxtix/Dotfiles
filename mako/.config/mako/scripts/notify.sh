b780076b6a1ecada46cba65534fca5659e05fbc4
Antonin Brauer <antonin.brauer@yahoo.fr>
2026-02-01 15:59:08 +0100

    modified notify usage

--- mako/.config/mako/scripts/notify.sh
+++ mako/.config/mako/scripts/notify.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
 
-# Usage: notify.sh LEVEL MESSAGE OVERRIDE_ICON
+# Usage: notify.sh TITLE BODY LEVEL OVERRIDE_ICON
 
-case "$2" in
+case "$3" in
     log)
 	    CATEGORY=log
 	    URGENCY=low
-	    ICON=${3:-""};;
+	    ICON=${4:-""};;
     info)
 	    CATEGORY=info
 	    URGENCY=low
-	    ICON=${3:-"/usr/share/icons/hicolor/scalable/status/info-circle-solid.svg"};;
+	    ICON=${4:-"/usr/share/icons/hicolor/scalable/status/info-circle-solid.svg"};;
     warning)
 	    CATEGORY=warning
 	    URGENCY=normal
-	    ICON=${3:-"/usr/share/icons/hicolor/scalable/status/warning-triangle-solid.svg"};;
+	    ICON=${4:-"/usr/share/icons/hicolor/scalable/status/warning-triangle-solid.svg"};;
     error) 
 	    CATEGORY=error
 	    URGENCY=critical
-	    ICON=${3:-"/usr/share/icons/hicolor/scalable/status/xmark-circle-solid.svg"};;
+	    ICON=${4:-"/usr/share/icons/hicolor/scalable/status/xmark-circle-solid.svg"};;
     critical)
 	    CATEGORY=critical
 	    URGENCY=critical
-	    ICON=${3:-"/usr/share/icons/hicolor/scalable/status/warning-circle-solid.svg"};;
+	    ICON=${4:-"/usr/share/icons/hicolor/scalable/status/warning-circle-solid.svg"};;
     *)
 	    CATEGORY=neutral
 	    URGENCY=normal
-	    ICON=${3:-""};;
+	    ICON=${4:-""};;
 esac
 
-MESSAGE="$1"
-shift 3
+TITLE="$1"
+BODY="$2"
+shift 4
 
-echo $@
-
-notify-send -c $CATEGORY -u $URGENCY "$MESSAGE" -i "$ICON" "$@"
+notify-send -c $CATEGORY -u $URGENCY "$TITLE" "$BODY" -i "$ICON" "$@"
