ea52fcbc9b707bb10e6dbe96fbc9184de0826649
Antonin Brauer <antonin.brauer@yahoo.fr>
2026-02-21 18:14:44 +0100

    fix wrong calling arguments for battery notifications

--- waybar/.config/waybar/scripts/battery_status.sh
+++ waybar/.config/waybar/scripts/battery_status.sh
@@ -11,15 +11,15 @@ PREVIOUS_LEVEL=$(cat ~/.config/waybar/scripts/cache/battery_level.cache)
 echo "$LEVEL" > ~/.config/waybar/scripts/cache/battery_level.cache
 
 if [ $(echo "$LEVEL < 50 && $PREVIOUS_LEVEL >= 50" | bc) = "1" ]; then
-	~/.config/mako/scripts/notify.sh "Battery level bellow 50%" warning "/usr/share/icons/Adwaita/symbolic/status/battery-caution-symbolic.svg" -a ""
+	~/.config/mako/scripts/notify.sh "Battery level bellow 50%" "" warning "/usr/share/icons/Adwaita/symbolic/status/battery-caution-symbolic.svg" -a ""
 fi
 
 if [ $(echo "$LEVEL < 30 && $PREVIOUS_LEVEL >= 30" | bc) = "1" ]; then
-	~/.config/mako/scripts/notify.sh "Battery level bellow 30%" warning "/usr/share/icons/Adwaita/symbolic/status/battery-caution-symbolic.svg" -a ""
+	~/.config/mako/scripts/notify.sh "Battery level bellow 30%" "" warning "/usr/share/icons/Adwaita/symbolic/status/battery-caution-symbolic.svg" -a ""
 fi
 
 if [ $(echo "$LEVEL < 10 && $PREVIOUS_LEVEL >= 10" | bc) = "1" ]; then
-	~/.config/mako/scripts/notify.sh "Battery level bellow 10%" critical "/usr/share/icons/Adwaita/symbolic/status/battery-action-symbolic.svg" -a ""
+	~/.config/mako/scripts/notify.sh "Battery level bellow 10%" "" critical "/usr/share/icons/Adwaita/symbolic/status/battery-action-symbolic.svg" -a ""
 fi
 
 if [ $PLUGGED = "0" ]; then
